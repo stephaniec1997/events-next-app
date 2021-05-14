f20aabf6a8be8b6489fed9c17db685e17144dc8b
Stephanie Castaneda <stephaniec1997@yahoo.com>
2021-05-13 18:00:25 -0700

    Add Menu functionality

---
+++ components/menu-button.js
@@ -0,0 +1,90 @@
+import {useState, useRef, useEffect} from 'react';
+import Link from 'next/link'
+import IconButton from '@material-ui/core/IconButton';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const MenuButton = () => {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = useRef(open);
+
+
+  return (
+    <div>
+      <div>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <Link href="/signin">
+                      <MenuItem onClick={handleClose}>
+                        Sign In
+                      </MenuItem>
+                    </Link>
+
+                    {/*<MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>*/}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </div>
+  );
+}
+
+export default MenuButton;
--- components/navbar.js
+++ components/navbar.js
@@ -1,20 +1,17 @@
+import Link from 'next/link'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import { makeStyles } from '@material-ui/core/styles';
 
-import ThemeSwitch from 'components/theme-switch'
+import MenuButton from 'components/menu-button';
+import ThemeSwitch from 'components/theme-switch';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
   titleContainer: {
     flexGrow: 1,
@@ -31,15 +28,15 @@ const Navbar = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Container className={classes.titleContainer}>
-            <Typography variant="h6" >
-              Events
-            </Typography>
-            <EventNoteIcon />
-          </Container>
+          <MenuButton />
+          <Link href="/">
+            <Container className={classes.titleContainer}>
+              <Typography variant="h6" >
+                Events
+              </Typography>
+              <EventNoteIcon />
+            </Container>
+          </Link>
           <ThemeSwitch />
         </Toolbar>
       </AppBar>
